d24c299a8d89f74acb162febdb6e52e95cf63755
Magnus Edenhill <magnus@edenhill.se>
2018-05-30 11:47:58 +0200

    Change for_api from string to rd_kafka_admin_op_t

--- tests/test.c
+++ tests/test.c
@@ -3291,7 +3291,7 @@ static void test_admin_create_topic (rd_kafka_t *use_rk,
                                         errstr, sizeof(errstr));
         TEST_ASSERT(newt[0] != NULL, "%s", errstr);
 
-        options = rd_kafka_AdminOptions_new(rk, "CreateTopics");
+        options = rd_kafka_AdminOptions_new(rk, RD_KAFKA_ADMIN_OP_CREATETOPICS);
         err = rd_kafka_AdminOptions_set_operation_timeout(options, timeout_ms,
                                                           errstr,
                                                           sizeof(errstr));
@@ -4032,7 +4032,7 @@ test_CreateTopics_simple (rd_kafka_t *rk,
                             topics[i], num_partitions, i, errstr);
         }
 
-        options = rd_kafka_AdminOptions_new(rk, "CreateTopics");
+        options = rd_kafka_AdminOptions_new(rk, RD_KAFKA_ADMIN_OP_CREATETOPICS);
         rd_kafka_AdminOptions_set_opaque(options, opaque);
 
         if (!useq) {
@@ -4096,7 +4096,8 @@ test_CreatePartitions_simple (rd_kafka_t *rk,
                     "Failed to NewPartitions(\"%s\", %"PRIusz"): %s",
                     topic, total_part_cnt, errstr);
 
-        options = rd_kafka_AdminOptions_new(rk, "CreatePartitions");
+        options = rd_kafka_AdminOptions_new(rk,
+                                            RD_KAFKA_ADMIN_OP_CREATEPARTITIONS);
         rd_kafka_AdminOptions_set_opaque(options, opaque);
 
         if (!useq) {
@@ -4159,7 +4160,7 @@ test_DeleteTopics_simple (rd_kafka_t *rk,
                 TEST_ASSERT(del_topics[i]);
         }
 
-        options = rd_kafka_AdminOptions_new(rk, "DeleteTopics");
+        options = rd_kafka_AdminOptions_new(rk, RD_KAFKA_ADMIN_OP_DELETETOPICS);
         rd_kafka_AdminOptions_set_opaque(options, opaque);
 
         if (!useq) {
@@ -4309,7 +4310,7 @@ rd_kafka_resp_err_t test_delete_all_test_topics (int timeout_ms) {
 
         q = rd_kafka_queue_get_main(rk);
 
-        options = rd_kafka_AdminOptions_new(rk, "DeleteTopics");
+        options = rd_kafka_AdminOptions_new(rk, RD_KAFKA_ADMIN_OP_DELETETOPICS);
         if (rd_kafka_AdminOptions_set_operation_timeout(options, 2*60*1000,
                                                         errstr,
                                                         sizeof(errstr)))
